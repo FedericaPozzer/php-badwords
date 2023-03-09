60b097a0c3e0d6f1c39e2670c87c2c7fc910f34b
Federica Pozzer <federica.pozzer@gmail.com>
2023-03-09 17:38:56 +0100

    init

---
+++ censor.php
@@ -0,0 +1,22 @@
+<?php 
+
+
+    // <h2>Il testo originale è:</h2>
+
+    echo $_GET["textarea"]
+
+
+
+    // <h2>Lunghezza testo originale:</h2>
+
+    echo strlen("textarea")
+
+
+
+    // <h2>Il testo censurato è:</h2>
+
+
+
+    // <h2>Lunghezza testo censurato:</h2>
+
+    ?>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+    
+    <h2>Inserisici un testo</h2>
+    <form method="GET" action="./index2.html">
+        <textarea name="textarea" cols="30" rows="10"> </textarea>
+        <br>
+        <br>
+        <label for="word-to-censor">Parola da censurare</label>
+        <br>
+        <input type="text" id="word-to-censor" name="censoredWord">
+    </form>
+<?php
+echo "ciao";
+?>
+
+</body>
+</html>
